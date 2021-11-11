f520a9fca54e8d8ba53405081199e70c72142efc
sheloholes <82578179+sheloholes@users.noreply.github.com>
2021-11-11 23:56:58 +0530

    Add files via upload

---
+++ Rialto_Solutions_Task/app.js
@@ -0,0 +1,6 @@
+var input=document.querySelector('#sbw span input[placeholder]')
+console.log(input["placeholder"]);
+// input.addEventListener("key",()=>{
+//     console.log(input.value);
+// })
+input["placeholder"].document.style.color="red"
